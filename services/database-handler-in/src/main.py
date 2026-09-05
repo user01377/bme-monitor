eaf7458b340b90bad2b42f18c1513492b60cdff6
Vincent Vue <vuevincent2025@gmail.com>
2026-09-05 14:25:15 -0400

    Implemented base main function for data handler in

--- services/database-handler-in/src/main.py
+++ services/database-handler-in/src/main.py
@@ -1,3 +1,17 @@
+import logging
+import logging.config
+import yaml
+
 from .redis_client import connect_redis
 
+with open("logging.yaml") as f:
+    logging.config.dictConfig(yaml.safe_load(f))
+
+logger = logging.getLogger(__name__)
+
 redis = connect_redis
+
+while True:
+    logger.info("Service started, querying REDIS for jobs..")
+
+    
