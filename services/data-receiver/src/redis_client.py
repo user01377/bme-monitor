d3a472d7c442187b626f4aab04ad753598b1dae0
Vincent Vue <vuevincent2025@gmail.com>
2026-09-05 05:10:06 -0400

    Implemented the redis client file

---
+++ services/data-receiver/src/redis_client.py
@@ -0,0 +1,39 @@
+import os
+import logging
+import asyncio
+from redis.asyncio import Redis
+from redis.exceptions import ConnectionError
+
+logger = logging.getLogger(__name__)
+
+def create_redis():
+    # creates redis object for connection
+    return Redis(
+        host="redis",
+        port=6379,
+        password=os.environ['REDIS_PASSWORD']
+    )
+
+async def connect_redis():
+    # connects to redis container with retry logic
+    redis = create_redis()
+
+    for i in range(1, 6):
+        try:
+            logger.info("Attempting connection to Redis...")
+            await redis.ping()
+            logger.info("Redis connected.")
+            return redis
+
+        except ConnectionError:
+            delay = i * 2
+
+            logger.error(
+                "Redis connection error, retrying in %s seconds",
+                delay,
+            )
+
+            await asyncio.sleep(delay)
+
+    await redis.aclose()
+    raise RuntimeError("Redis connection failed.")
