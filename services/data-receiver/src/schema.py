17df9a26d9fd08a848bc0425fafebdedeb49af1e
Vincent Vue <vuevincent2025@gmail.com>
2026-09-05 12:27:44 -0400

    Updated receiver in schema for strings instead of bytes

--- services/data-receiver/src/schema.py
+++ services/data-receiver/src/schema.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 class ReceiverIn(BaseModel):
     device_id: str
     timestamp: int
-    data: bytes
-    signature: bytes
+    data: str
+    signature: str
 
 class ReceiverResponse(BaseModel):
     status: str
