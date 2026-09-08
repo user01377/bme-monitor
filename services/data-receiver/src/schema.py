44e74530f7a295b60d0fb38fd89221a36a099be3
Vincent Vue <vuevincent2025@gmail.com>
2026-09-07 20:41:22 -0400

    Fixed model creation error due to declaration order of schema

--- services/data-receiver/src/schema.py
+++ services/data-receiver/src/schema.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel
 
+class SensorData:
+    temperature: float
+    humidity: float
+    pressure: float
+
 class ReceiverIn(BaseModel):
     device_id: str # unique id for node sending data
     timestamp: int # unix time stamp
     data: SensorData
     signature: str
-
-class SensorData:
-    temperature: float
-    humidity: float
-    pressure: float
 
 class ReceiverResponse(BaseModel):
     status: str
