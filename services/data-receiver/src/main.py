89139e8464dc7072d76e13e54658266e2c0e2bd5
Vincent Vue <vuevincent2025@gmail.com>
2026-09-08 17:20:32 -0400

    Removed sql table creation from data-receiver main

--- services/data-receiver/src/main.py
+++ services/data-receiver/src/main.py
@@ -2,13 +2,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .redis_client import connect_redis
 from .routes import router
-from .database import engine
-from .base import Base
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    Base.metadata.create_all(engine)
-
     redis = await connect_redis()
 
     app.state.redis = redis
