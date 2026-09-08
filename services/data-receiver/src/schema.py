1ca4bf846389a7f2ee0d3479922420b686c31d99
Vincent Vue <vuevincent2025@gmail.com>
2026-09-07 20:09:20 -0400

    Updated ReceiverIn schema for receiver service

--- services/data-receiver/src/schema.py
+++ services/data-receiver/src/schema.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 
 class ReceiverIn(BaseModel):
-    device_id: str
-    timestamp: int
-    data: str
+    device_id: str # unique id for node sending data
+    timestamp: int # unix time stamp
+    data: SensorData
     signature: str
+
+class SensorData:
+    temperature: float
+    humidity: float
+    pressure: float
 
 class ReceiverResponse(BaseModel):
     status: str
