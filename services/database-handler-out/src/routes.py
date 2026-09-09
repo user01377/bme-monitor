46f1d0ce746c24de8032ebf523928b920bcfd227
Vincent Vue <vuevincent2025@gmail.com>
2026-09-08 22:28:27 -0400

    Implemented route to get average of all telemetry data

--- services/database-handler-out/src/routes.py
+++ services/database-handler-out/src/routes.py
@@ -1,7 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session
+
+from .database import get_db
+from .models import SensorReads, Status
 
 router = APIRouter()
 
 @router.get("/health")
 def get_health():
     return {"status": "ok"}
+
+@router.get("/telemetry/average")
+def get_data(db: Session = Depends(get_db)):
+    """
+    Returns AVG of each data point
+    """
+
+    stmt = select(
+    func.avg(SensorReads.temp).label("temperature"),
+    func.avg(SensorReads.humidity).label("humidity"),
+    func.avg(SensorReads.pressure).label("pressure"),
+    ).where(
+        SensorReads.status.in_([
+            Status.VALID,
+            Status.MANUAL_INC
+        ])
+    )
+
+    result = db.execute(stmt).one()
+
+    return {
+        "temperature": result.temperature,
+        "humidity": result.humidity,
+        "pressure": result.pressure,
+    }
