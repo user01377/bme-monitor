e00b9861d19d141c8f1b7f5bd417eeec87790602
Vincent Vue <vuevincent2025@gmail.com>
2026-09-08 21:57:31 -0400

    Added base python files for data-handler-out service

---
+++ services/database-handler-out/src/database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+database_url = f"postgresql+psycopg://{user}:{password}@db:5432/bme280"
+
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+def get_db():
+    pass
