b580e123d580b58f3b3cfcba149acc86acd254c5
Vincent Vue <vuevincent2025@gmail.com>
2026-09-09 11:19:08 -0400

    Implemented user auth function and added it as dependency for all routes

--- services/data-server/src/routes.py
+++ services/data-server/src/routes.py
@@ -1,6 +1,28 @@
-from fastapi import APIRouter
+import hashlib
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import APIKeyHeader
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from .models import ApiToken
+from .database import get_db
 
-router = APIRouter()
+api_key = APIKeyHeader(name="X-API-Key")
+
+
+def hash_token(token: str):
+    return hashlib.sha256(token.encode()).hexdigest()
+
+def authenticate_user(token: str = Depends(api_key), db: Session = Depends(get_db)):
+    token_hash = hash_token(token)
+
+    api_token = db.scalar(select(ApiToken).where(ApiToken.token_hash == token_hash))
+
+    if not api_token:
+        raise HTTPException(status_code=401, detail="Invalid API Token.")
+    
+    return api_token
+
+router = APIRouter(dependencies=[Depends(authenticate_user)])
 
 @router.get("/health")
 def get_health():
