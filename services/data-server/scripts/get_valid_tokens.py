967ed257c5574688d1c9e70aa8193c63df31a10b
Vincent Vue <vuevincent2025@gmail.com>
2026-09-19 10:59:25 -0400

    Refactored both get invalid and valid script for formatting

--- services/data-server/scripts/get_valid_tokens.py
+++ services/data-server/scripts/get_valid_tokens.py
@@ -5,10 +5,19 @@ from sqlalchemy import select
 
 def get_valid_tokens():
     with Session(engine) as session:
-        names = session.scalars(select(ApiToken.name).where(ApiToken.revoked_at == None)).all()
+        tokens = session.scalars(select(ApiToken).where(ApiToken.revoked_at == None)).all()
 
-        for name in names:
-            print(name)
+        if not tokens:
+            print("NO TOKENS")
+            return
+
+        for token in tokens:
+            created = token.created_at.strftime('%Y-%m-%d %H:%M:%S UTC')
+
+            last_used = (token.last_used_at.strftime('%Y-%m-%d %H:%M:%S UTC') if token.last_used_at else None)
+
+            print(f"{token.name} | {created} | {last_used}")
 
 if __name__ == "__main__":
+    print("TOKEN_NAME | CREATED_AT | LAST_USED_AT")
     get_valid_tokens()
