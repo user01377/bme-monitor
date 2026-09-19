044cac954219a1c073d000437e575636326e7a3f
Vincent Vue <vuevincent2025@gmail.com>
2026-09-19 10:06:30 -0400

    Implemented data-server script to get all revoked tokens

---
+++ services/data-server/scripts/get_invalid_tokens.py
@@ -0,0 +1,14 @@
+from src.database import engine
+from src.models import ApiToken
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+def get_revoked_tokens():
+    with Session(engine) as session:
+        names = session.scalars(select(ApiToken.name).where(ApiToken.revoked_at != None)).all()
+
+        for name in names:
+            print(name)
+
+if __name__ == "__main__":
+    get_revoked_tokens()
